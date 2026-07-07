f94588bb466136f1519a490f26b0c3f26db4beb7
Eduardo Matheus <68598652+EduardoM-GO@users.noreply.github.com>
2026-07-07 00:16:41 +0000

    Ajuste no migration

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,33 +1,35 @@
-import migrationRunner from 'node-pg-migrate'
-import { join } from "node:path"
-import database from "infra/database.js"
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+import database from "infra/database.js";
 
 export default async function migrations(request, response) {
+  try {
+    if (!["GET", "POST"].includes(request.method)) {
+      return response.status(405).end();
+    }
 
-  if (!["GET", "POST"].includes(request.method)) {
-    return response.status(405).end();
+    const dbClient = await database.getNewClient();
+
+    const isDryRun = request.method === "GET";
+
+    const migrations = await migrationRunner({
+      dbClient: dbClient,
+      dryRun: isDryRun,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
+
+    if (migrations.length > 0 && !isDryRun) {
+      return response.status(201).json(migrations);
+    }
+
+    return response.status(200).json(migrations);
+  } catch (err) {
+    console.error(err);
+    throw err;
+  } finally {
+    await dbClient.end();
   }
-
-  const dbClient = await database.getNewClient();
-
-  const isDryRun = request.method === "GET"
-
-  const migrations = await migrationRunner({
-    dbClient: dbClient,
-    dryRun: isDryRun,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations"
-  })
-
-  await dbClient.end();
-
-  if (migrations.length > 0 && !isDryRun) {
-    return response.status(201).json(migrations);
-  }
-
-
-  return response.status(200).json(migrations);
 }
-
